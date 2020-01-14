88182fbaa482ae1ef885a28d92274c55a950a31c
yangchao <yangchao61@outlook.com>
2020-01-14 23:11:18 +0800

    add a mock

--- ut/BoxTest.cpp
+++ ut/BoxTest.cpp
@@ -4,10 +4,37 @@
 #include"../Box.h"
 #include"TestTrunkLoad.hpp"
 
+class Foo {
+ public:
+  virtual bool Transform(int m) = 0;
+
+ protected:
+  virtual void Resume() = 0;
+
+ private:
+  virtual int GetTimeOut() = 0;
+};
+
+class MockFoo : public Foo 
+{
+ public:
+  MOCK_METHOD1(Transform, bool(int m));
+
+  // The following must be in the public section, even though the
+  // methods are protected or private in the base class.
+  MOCK_METHOD0(Resume, void());
+  MOCK_METHOD0(GetTimeOut, int());
+  MockFoo(){};
+};
+
+
+
 // g++ BoxTest.cpp -lgtest -lgmock -lpthread to compile
 
 TEST(BoxTestShould, ReturnFaseWhwnBoxIsSmall)
 {
+    MockFoo foo;
+    // std::shared_ptr<MockFoo> mailService = std::make_shared<MockFoo>();
     Box box {1, 2, 5};
     Box another{1, 1, 1};
     EXPECT_EQ(box.GetSurface(), 34);
