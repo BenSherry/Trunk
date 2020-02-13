f04117458d7307dd64fb9160b1745a75986bd78e
jasoyang <jason.2.yang@nokia-sbell.com>
2020-02-13 22:10:22 +0800

    1. add some function for handle string 2 .add a function for calculator days between 2 date

--- Helper.hpp
+++ Helper.hpp
@@ -2,9 +2,146 @@
 
 #include<algorithm>
 #include<iostream>
+#include<string>
+#include<array>
+#include<numeric>
+
 template<typename T>
 void showData(T data)
 {
     std::for_each(data.begin(), data.end(), [](decltype(*data.begin()) element) {std::cout << element << " "; });
     std::cout<<"\n";
 }
+
+std::string strReplace(std::string sentence,
+    std::string src,
+    std::string dst)
+{
+    int pos = 0;
+    while ((pos = sentence.find(src, pos)) != std::string::npos)
+    {
+        sentence.replace(pos, src.length(), dst);
+        pos = pos + dst.length();
+    }
+    return sentence;
+}
+
+void split_byContent(std::string src,
+    std::string pattern,
+    std::vector<std::string>& vecctorResult)
+{
+    std::string::size_type lastpos = src.find_first_not_of(pattern, 0);
+    std::string::size_type pos = src.find_first_of(pattern, lastpos);
+    while (std::string::npos != pos || std::string::npos != lastpos)
+    {
+        vecctorResult.push_back(src.substr(lastpos, pos - lastpos));
+        lastpos = src.find_first_not_of(pattern, pos);
+        pos = src.find_first_of(pattern, lastpos);
+    }
+}
+
+void split_byLength(const std::string& src,
+    int special_length,
+    std::vector<std::string>& vecctorResult)
+{
+    int total_len = src.length();
+    int split_len = total_len / special_length;
+    int remainder = total_len % special_length;
+    if (remainder > 0) {
+        split_len = split_len + 1;
+    }
+
+    for (int i = 0; i < split_len; i++) {
+        vecctorResult.push_back(src.substr(i * special_length, special_length));
+    }
+}
+
+namespace TimeHelper
+{
+    class Time
+    {
+    public:
+        Time(int day, int month, int year) :
+            day_{ day },
+            month_{ month },
+            year_{ year },
+            isLeap_{ isLeapYear() }{};
+        Time(const Time& other)
+        {
+            day_ = other.getDay();
+            month_ = other.getMonth();
+            year_ = other.getYear();
+            isLeap_ = other.getLeap();
+        }
+        int getDay() const { return day_; };
+        int getMonth() const { return month_; };
+        int getYear() const { return year_; };
+        bool getLeap() const { return isLeap_; }
+        int operator-(const Time& other)
+        {
+            return TimeDiffBase() - other.TimeDiffBase();
+        }
+        int TimeDiffBase() const
+        {
+            /*******************************************************
+            make 0001-01-01 as base;
+            1.there is 97 leap years and 403 nonleap year in 400 years
+            century4Inyear * (97 * 366 + 303 * 365)
+    
+            2.getYear() % 400 is the rest part.
+            in this part,rest/4 - rest/100 is leap years
+    
+            nocice, after this,we get days about whole year, include
+            some uncome days in this year
+            ***********************************************************/
+            int century4Inyear = getYear() / 400;
+            int rest = getYear() % 400;
+            int leapsInRest = rest / 4 - rest / 100;
+            int nonleapInRest = rest - leapsInRest;
+            int daysofwholeYears = century4Inyear * (97 * 366 + 303 * 365) +
+                (leapsInRest * 366 + nonleapInRest * 365);
+            int daysInThisYear = 365;
+            if (getLeap())
+            {
+                daysInThisYear = 366;
+            }
+            int uncomeDay = daysInThisYear - getPastThisYear();
+            int day = daysofwholeYears - uncomeDay;
+    
+            return day;
+        }
+    private:
+        int getPastThisYear() const
+        {
+            std::array<int, 12> dayMonth{ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            if (getLeap())
+            {
+                dayMonth[1] = 29;
+            }
+            if (getMonth() == 1)
+            {
+                return getDay();
+            }
+            int pastDay = std::accumulate(dayMonth.begin(),
+                dayMonth.begin() + getMonth() - 1,
+                0);
+            return pastDay + getDay();
+        }
+        bool isLeapYear() const
+        {
+            if ((year_ % 400 == 0) ||
+                (year_ % 4 == 0 && year_ % 100 != 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        int day_;
+        int month_;
+        int year_;
+        bool isLeap_;
+    };
+}; // namespace TimeHelper
